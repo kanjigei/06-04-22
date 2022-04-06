e0b11b7c63ff318dc1dad32da6a1c1f1de51293d
kanjigei <102357901+kanjigei@users.noreply.github.com>
2022-04-06 13:03:40 +0100

    Add files via upload

---
+++ nea project/src/nea/project/main.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nea.project;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author 15choasi
+ */
+
+public class main extends window{
+    
+      public static void main(String[] args) {
+          
+        new window();
+        
+        
+        
+        
+        
+       // new editor();
+       
+       //Button btn = new button();
+       //button.setOnAction(e -> {
+       // System.out.pritn("hello);
+       //}
+        
+        
+    }
+ }
